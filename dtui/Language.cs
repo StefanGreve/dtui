9403cfd2b6b71804f00fb4253883dc9973df8e05
StefanGreve <greve.stefan@outlook.jp>
2022-04-13 15:58:53 +0200

    Add Spanish and Chinese constants and implement IsAsian function

--- dtui/Language.cs
+++ dtui/Language.cs
@@ -11,5 +11,11 @@ namespace dtui
         public const string English = "en_US";
 
         public const string Japanese = "ja_JP";
+
+        public const string Spanish = "es_ES"; // TODO
+
+        public const string Chinese = "zh_CH"; // TODO
+
+        public static bool IsAsian(string language) => language == Japanese || language == Chinese;
     }
 }
